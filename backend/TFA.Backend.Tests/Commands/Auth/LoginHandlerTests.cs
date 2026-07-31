5b8d1cf3e5468daaeab23d25f1be1f30ea1617ab
Daniel Wilches <swilches@toyotacredito.com.co>
2026-07-31 05:38:22 -0500

    Se crea el proyecto de pruebas y un par de pruebas para Login y Category

---
+++ backend/TFA.Backend.Tests/Commands/Auth/LoginHandlerTests.cs
@@ -0,0 +1,73 @@
+﻿using FluentAssertions;
+using Moq;
+using TFA.Backend.Application.Commands.Auth;
+using TFA.Backend.Application.Commands.CategoryCommand.CreateCategory;
+using TFA.Backend.Application.DTOs.Auth;
+using TFA.Backend.Application.Interfaces.Auth;
+using TFA.Backend.Domain.Entities;
+using TFA.Backend.Domain.Repositories;
+using Xunit;
+
+namespace TFA.Backend.Tests.Commands.Auth
+{
+    public class LoginHandlerTests
+    {
+        [Fact]
+        public async Task Handle_ShouldLoginUser()
+        {
+            // Arrange
+            var userRepository = new Mock<IUserRepository>();
+            var tokenService = new Mock<ITokenService>();
+
+            userRepository
+                .Setup(x => x.ValidateLogin(
+                    It.IsAny<string>(),
+                    It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new User
+                {
+                    Name = "admin",
+                    Username = "admin",
+                    Password = "123456"
+                });
+
+            tokenService
+                .Setup(x => x.GenerateToken(
+                    It.IsAny<User>()))
+                .Returns(new TokenDto
+                {
+                    Token = "FAKE_TOKEN",
+                    Type = "Bearer",
+                    Expire = 599
+                });
+
+            var handler = new LoginCommandHandler(userRepository.Object, tokenService.Object);
+
+            var command = new LoginCommand
+            (
+                "admin",
+                "123456"
+            );
+
+
+            // Act
+            var result = await handler.Handle(command);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Token.Should().Be("FAKE_TOKEN");
+            result.Type.Should().Be("Bearer");
+            result.Expire.Should().Be(599);
+
+            userRepository.Verify(
+                x => x.ValidateLogin(
+                    It.IsAny<string>(),
+                    It.IsAny<CancellationToken>()),
+                Times.Once());
+
+            tokenService.Verify(
+                x => x.GenerateToken(
+                    It.IsAny<User>()),
+                Times.Once());
+        }
+    }
+}
---
+++ backend/TFA.Backend.Tests/Commands/Category/CreateCategoryHandlerTests.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using Moq;
+using TFA.Backend.Application.Commands.CategoryCommand.CreateCategory;
+using TFA.Backend.Domain.Entities;
+using TFA.Backend.Domain.Repositories;
+using Xunit;
+
+namespace TFA.Backend.Tests.Commands.Category
+{
+    public class CreateCategoryHandlerTests
+    {
+        [Fact]
+        public async Task Handle_ShouldCreateCategory()
+        {
+            // Arrange
+            var repository = new Mock<ICategoryRepository>();
+
+            repository
+                .Setup(x => x.CreateCategory(
+                    It.IsAny<Domain.Entities.Category>(),
+                    It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new Domain.Entities.Category
+                {
+                    CategoryName = "SERVIDORES",
+                    Description = "Test Description",
+                    Picture = "photo.png"
+                });
+
+            var handler = new CreateCategoryHandler(repository.Object);
+
+            var command = new CreateCategoryCommand
+            (
+                Guid.NewGuid(),
+                "SERVIDORES", 
+                "Test Description",
+                "photo.png"
+            );
+
+            //Act
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            //Assert
+            result.Should().NotBeNull();
+
+            result.CategoryName.Should().Be("SERVIDORES");
+        }
+    }
+}
